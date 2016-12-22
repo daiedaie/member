77b15098bdb786259e401ec7f7e07d02c6e69f7e
guojia <dmcguojia@qq.com>
2016-12-22 11:11:55 +0800

    修改打包配置

--- member/member-common/src/main/java/com/paytong/platform/member/commons/utils/LogHelper.java
+++ member/member-common/src/main/java/com/paytong/platform/member/commons/utils/LogHelper.java
@@ -8,7 +8,7 @@
  * Copyright (c) 2016,zlebank.All rights reserved.
  * 
  */
-package com.paytong.platform.member.individual.aop;
+package com.paytong.platform.member.commons.utils;
 
 import java.lang.reflect.Method;
 
